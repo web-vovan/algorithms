21fb6ee08b15c3dc248371fd8a9caf38fe6d77b6
vladimir <myweb11@mail.ru>
2021-05-30 19:17:40 +0300

    алгоритм Евклида

---
+++ fast_sort/euclidean_algorithm.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * алгоритм Евклида для нахождения наибольшего общего делителя двух чисел
+ * также может использоваться на нахождения стороны наибольшего квадрата
+ * при делении произвольного участка на равные квадраты
+*/
+function euclidean_algorithm($a, $b) {
+    if ($a > $b) {
+        if ($a % $b === 0) {
+            return $b;
+        }
+
+        return euclidean_algorithm($a % $b, $b);
+    }
+
+    if ($a < $b) {
+        if ($b % $a === 0 ) {
+            return $a;
+        }
+
+        return euclidean_algorithm($a, $b % $a);
+    }
+}
+
+echo euclidean_algorithm(9, 12); // 3
+echo euclidean_algorithm(17, 88); // 1
+echo euclidean_algorithm(1680, 640); // 80
