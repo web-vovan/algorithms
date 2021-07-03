6d056624e945bbeac763d1e688d135c6f85c6e62
vladimir <myweb11@mail.ru>
2021-07-03 22:02:45 +0300

    граф на php

--- Дополнительно/граф на php/index.php
+++ Дополнительно/граф на php/index.php
@@ -23,6 +23,12 @@ $graph->addEdge('A', 'B', 3);
 $graph->addEdge('A', 'end', 1);
 $graph->addEdge('B', 'end', 5);
 
+// перечисление всех вершин
+foreach ($graph->getNodes() as $node) {
+    echo $node . PHP_EOL;
+}
+
+// все ребра для вершины start
 $node1 = 'start';
 foreach ($graph->getEdges($node1) as $node2 => $length) {
     echo $node1 . ' - ' . $node2 . ': ' . $length . PHP_EOL;
